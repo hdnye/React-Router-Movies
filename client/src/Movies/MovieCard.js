6b48b350ef8a40b1a3ae6c0e0bb4ec7e7d104cfa
Heather Nye <heatherdnye@gmail.com>
2019-10-30 20:26:10 -0500

    day 2 stucture

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+const movieData = [];
+
 const MovieCard = props => {
-  return;
-};
+    const movieId = movieData.find(movieId => props.match.params.movieId === `${movieId.id}`);
+     return (
+      <h2>{movieId.name}</h2>
+   );
+}
 
 export default MovieCard;
